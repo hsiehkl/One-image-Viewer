75088b31a6688077e4ca2ccdb643ade1ec040e1b
hsiehkl <st4465@gmail.com>
2017-11-23 16:11:48 +0800

    able to zoom

--- One-image-Viewer/ViewController.swift
+++ One-image-Viewer/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var butoonView: UIView!
     var scrollView: UIScrollView!
     var imageView: UIImageView!
-    var containerView : UIView!
     @IBOutlet weak var palceholderImageView: UIImageView!
     
     override func viewDidLoad() {
@@ -21,33 +21,70 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         //setup placeholder image
         palceholderImageView.image = UIImage(named: "icon_photo")?.withRenderingMode(.alwaysTemplate)
         palceholderImageView.tintColor = UIColor.white
-        imageView = UIImageView(image: UIImage(named: "icon_photo"))
+        imageView = UIImageView(image: UIImage(named: "02"))
         
         scrollView = UIScrollView()
         scrollView.delegate = self
-        scrollView.contentSize = CGSize(width: 375, height: 590)
+        scrollView.frame = CGRect(x: 0, y: 0, width: 375, height: 100)
+        scrollView.contentSize = imageView.bounds.size
         
-        containerView = UIView()
-        
-        scrollView.minimumZoomScale = 1.0
+        scrollView.minimumZoomScale = 0.0
         scrollView.maximumZoomScale = 2.0
         scrollView.bounces = false
         
-        containerView.addSubview(imageView)
-        scrollView.addSubview(containerView)
+        scrollView.addSubview(imageView)
+//        scrollView.addSubview(butoonView)
         view.addSubview(scrollView)
         
+    }
+    
+    func centerScrollViewContents(){
+        let boundsSize = scrollView.bounds.size
+        var contentsFrame = imageView.frame
+
+        if contentsFrame.size.width < boundsSize.width {
+            contentsFrame.origin.x = (boundsSize.width - contentsFrame.size.width) / 2
+        }
+        else {
+            contentsFrame.origin.x = 0
+        }
+
+        if contentsFrame.size.height < boundsSize.height {
+            contentsFrame.origin.y = ((boundsSize.height - 77.0) - contentsFrame.size.height) / 2
+        }
+        else {
+            contentsFrame.origin.y = 0
+        }
+
+        imageView.frame = contentsFrame
+    }
+
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerScrollViewContents()
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
     
+    fileprivate func updateMinZoomScaleForSize(_ size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        
+        let minScale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = minScale
+        
+        scrollView.zoomScale = minScale
+        
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
+        updateMinZoomScaleForSize(view.bounds.size)
+        
         scrollView.frame = view.bounds
-        containerView.frame = CGRect(x: 0, y: 0, width: scrollView.contentSize.width, height: scrollView.contentSize.height)
+
     }
 
     override func didReceiveMemoryWarning() {
